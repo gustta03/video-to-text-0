2270b50084bbad53572cbad6a0cd4fd74f32962e
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-12 17:53:41 -0300

    use dotenv config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,9 @@
 import { setupRoutes } from '@/main/config/routes'
 import cors from 'cors'
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 app.use(cors({
