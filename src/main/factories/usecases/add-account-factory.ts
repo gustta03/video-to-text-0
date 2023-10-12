020864afa3bb8152b0c7c76b308bcbd96191a651
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-12 16:56:31 -0300

    rename class emailValidator

--- src/main/factories/usecases/add-account-factory.ts
+++ src/main/factories/usecases/add-account-factory.ts
@@ -4,12 +4,12 @@ import { TokenGenerator } from '@/infra/cryptography/token-generator'
 import { AddAccountRepository } from '@/infra/repositories/user/user-repository'
 import env from '@/main/config/env'
 import { AddAccountUseCase } from '@/usecases/add-account'
-import { EmailValidator } from '@/utils/email-validator'
+import { Validator } from '@/utils/email-validator'
 
 export const makeAddAccountUseCase = () => {
   const addAccountRepository = new AddAccountRepository()
   const hasher = new BcryptHashAdapter(10)
-  const emailVerify = new EmailValidator()
+  const emailVerify = new Validator()
   const generateToken = new TokenGenerator(env.jwtSecret)
   return new AddAccountUseCase(addAccountRepository, emailVerify, hasher, generateToken)
 }
