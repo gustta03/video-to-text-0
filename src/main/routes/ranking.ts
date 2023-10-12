971a84573551fcd25762a2327d6b60fdd13bbb24
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-12 12:21:34 -0300

    deleted

--- src/main/routes/ranking.ts
+++
@@ -1,8 +0,0 @@
-import { makeTranscription } from '@/main/factories/controller/make-trascription-controller'
-import { adaptRoute } from '@/main/adapters'
-
-import { Router } from 'express'
-
-export default (router: Router): void => {
-  router.get('/transcription/video/:id', adaptRoute(makeTranscription()))
-}
