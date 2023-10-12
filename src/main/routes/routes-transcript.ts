f2a209ec6fcf1913066932caaf980984e39d9968
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-12 17:52:07 -0300

    add route to get meaning word from GPT

--- src/main/routes/routes-transcript.ts
+++ src/main/routes/routes-transcript.ts
@@ -1,9 +1,9 @@
 import { makeTranscription } from '@/main/factories/controller/make-trascription-controller'
 import { adaptRoute } from '@/main/adapters'
-
 import { Router } from 'express'
-// import { makeAddAccountController } from '../factories/controller/add-account-controller'
+import { makeGetMeaningController } from '../factories/controller/make-get-meaning-controller'
 
 export default (router: Router): void => {
   router.get('/transcription/video/:id', adaptRoute(makeTranscription()))
+  router.get('/meaning/word/:word', adaptRoute(makeGetMeaningController()))
 }
