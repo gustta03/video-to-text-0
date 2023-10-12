916fb8830c5690feb2000780edbc8fd058519384
Gustavo Alves <gustavo_alves777@outlook.com>
2023-10-12 16:55:46 -0300

    get env API key openIA

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWTSECRET || 'secret',
-  mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/default_database_name'
+  mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/default_database_name',
+  openIAKey: process.env.OPEN_IA_API_KEY
 }
